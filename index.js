2c1e5bbfa02743353affeb78c9e5d2651834e53f
Jazz158 <jahanzaibk246@gmail.com>
2026-05-08 12:13:12 +0500

    phonebook 3.2 - how many entries at current time

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ app.get("/api/persons",(request,response) => {
     response.json(data)
 })
 
+app.get("/info",(request,response) => {
+
+    response.send((`
+    <p>Phonebook has info for ${data.length} people</p>
+    <p>${new Date()}</p>
+  `))
+})
+
 const Port = 3001 
 
 app.listen(Port,() => {
